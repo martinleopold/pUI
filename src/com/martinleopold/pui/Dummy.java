a0e207e7f0089f2327e5bcae2daf9b25967236c2
Martin Leopold <m@martinleopold.com>
2013-08-30 21:56:45 +0200

    renamed

---
+++ src/com/martinleopold/pui/Dummy.java
@@ -0,0 +1,80 @@
+/*
+ * Copyright (C) 2013 Martin Leopold <m@martinleopold.com>
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ */
+package com.martinleopold.pui;
+
+import processing.core.PApplet;
+
+/**
+ * A Widget to test Events
+ * @author Martin Leopold <m@martinleopold.com>
+ */
+public class Dummy extends Widget {
+
+	public Dummy(PUI pui, float x, float y, float width, float height) {
+		super(pui, x, y, width, height);
+	}
+
+	@Override
+	public void mouseEntered(float mx, float my) {
+		System.out.println("entered: mx:" + mx + " my:" + my);
+	}
+
+	@Override
+	public void mouseExited(float mx, float my) {
+		System.out.println("exited: mx:" + mx + " my:" + my);
+	}
+
+	@Override
+	public void mouseMoved(float mx, float my) {
+		System.out.println("moved: mx:" + mx + " my:" + my);
+	}
+
+	@Override
+	public void mousePressed(int button, float mx, float my) {
+		System.out.println("pressed: button:" + button + " mx:" + mx + " my:" + my);
+	}
+
+	@Override
+	public void mouseReleased(int button, float mx, float my) {
+		System.out.println("released: button:" + button + " mx:" + mx + " my:" + my);
+	}
+
+	@Override
+	public void mouseClicked(int button, int count, float mx, float my) {
+		System.out.println("clicked: count:" + count + " button:" + button + " mx:" + mx + " my:" + my);
+	}
+
+	@Override
+	public void mouseDragged(int button, float mx, float my, float dx, float dy) {
+		System.out.println("dragged: button:" + button + " mx:" + mx + " my:" + my + " dx:" + dx + " dy:" + dy);
+	}
+
+	@Override
+	public void mouseScrolled(int amount) {
+		System.out.println("scrolled: amount:" + amount);
+	}
+
+	@Override
+	public void draw(PApplet p) {
+		p.pushStyle();
+		p.fill(64);
+		p.stroke(128);
+		p.rect(x, y, width, height);
+		p.popStyle();
+	}
+}
